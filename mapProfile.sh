f7a85e5408d21d0ffa1c1229e353725799162513
SRenan <renan.sauteraud@gmail.com>
2012-07-16 13:05:53 -0700

    Deleted proMapAll.sh Updated README

--- mapProfile.sh
+++ mapProfile.sh
@@ -2,9 +2,8 @@
 ###
 # Description:
 #   This script generates mappability profiles to be used in PICS and PING packages
-#   ref: 
 #
-# 06 July 2012
+# 16 July 2012
 ###
 
 
@@ -12,13 +11,15 @@ usage="
 Usage: mapProfile.sh -l readLength -g genomeName [options]
 
 Options:
--d : Directory where the files will be downloaded and extracted.
 -c : Specify a chromosome to download. If the entire genome is not needed.
 -h : Print this help message and exit.
+
+Examples:
+./mapProfile.sh -l 50 -g hg19          #For the entire human genome with 50bp reads
+./mapProfile.sh -l 36 -g sacCer2 -c I  #For the chromosome I of saccharomyces cerevisiae with 36bp reads
 "
 
 scriptdir=`dirname $0`
-workdir=./
 genome=
 length=
 chr="*" #default is all chr selected
@@ -34,7 +35,6 @@ do
     -l|--length) length=$2;shift;;
     -g|--genome) genome=$2;shift;;
     -c|--chr) chr=$2;shift;;
-    -d|--dir) workdir=$2;shift;;
     -h|--h|-help|--help) printHelp; exit 1;;
     -*|--*) echo "Error! Wrong argument $1"; printHelp; exit 1;;
   esac
@@ -53,10 +53,10 @@ if [ "$length" == "" ]
 fi
 
 # Get the fasta for each chromosome
-wget -P $workdir  --timestamping "ftp://hgdownload.cse.ucsc.edu/goldenPath/${genome}/chromosomes/chr${chr}.fa.gz" #-P to specify DL directory
+wget --timestamping "ftp://hgdownload.cse.ucsc.edu/goldenPath/${genome}/chromosomes/chr${chr}.fa.gz" #-P to specify DL directory
 
 # Unzip
-gunzip $workdir/*fa.gz
+gunzip *fa.gz
 # Merge
 cat *.fa > $genome.fa
 
@@ -65,11 +65,18 @@ bwa index -a bwtsw $genome.fa
 samtools faidx $genome.fa
 
 # pileup and bedFile creation
-#python pilup.py 
-$scriptdir/proMapAll.sh -l $length -g $genome.fa
 if [ "$chr" == "*" ]
 then
+  for i in chr*.fa
+  do
+    $scriptdir/proMap.sh -l $length -g $genome.fa -c $i #& #run on every cores simultaneously
+  done
+  cat chr*.fa-$length/chr*.fa-$length.bed > $genome-$length.bed
+  #clean dir
+  rm -r chr*.fa-$length
+  rm -r $genome.fa.*
   echo "Mappability profile for the genome $genome with reads of length $length: $genome-$length.bed"
 else
-  echo "Mappability profile for chr$chr of the genome $genome with reads of length $length: $genome-$length.bed"
+  $scriptdir/proMap.sh -l $length -g $genome.fa -c chr$chr.fa
+  echo "Mappability profile for chr$chr of the genome $genome with reads of length $length: chr$chr.fa-$length/chr$chr.fa-$length.bed"
 fi
