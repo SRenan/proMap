30a549e24ab48706d3565906cd516a84b8c0560d
SRenan <renan.sauteraud@gmail.com>
2013-11-01 11:59:10 -0700

    stuff

--- proMap.sh
+++ proMap.sh
@@ -51,29 +51,29 @@ mkdir $chr-$length
 perl "$perl_dir/enumeratenmer" -nmer $length -skip_gappy -fasta=$chr -raw -fastq > "$chr-$length/$chr-$length+.txt" 2> "$chr-$length/$chr-$length-.txt"
 bwa aln $genome "$chr-$length/$chr-$length+.txt" > "$chr-$length/$chr-$length.sai"
 bwa samse $genome "$chr-$length/$chr-$length.sai" "$chr-$length/$chr-$length+.txt" > "$chr-$length/$chr-$length.sam"
-#cleanup
-rm $chr-$length/$chr-$length*.txt
-rm $chr-$length/$chr-$length*.sai
+##cleanup
+#rm $chr-$length/$chr-$length*.txt
+#rm $chr-$length/$chr-$length*.sai
 grep "XT:A:U" "$chr-$length/$chr-$length.sam" > "$chr-$length/$chr-$length-U.sam"
-#cleanup
-rm $chr-$length/$chr-$length.sam
+##cleanup
+#rm $chr-$length/$chr-$length.sam
 samtools view -b -S -t "$genome.fai" -o "$chr-$length/$chr-$length.bam" "$chr-$length/$chr-$length-U.sam"
-#cleanup
-rm $chr-$length/$chr-$length-U.sam
+##cleanup
+#rm $chr-$length/$chr-$length-U.sam
 samtools sort "$chr-$length/$chr-$length.bam" "$chr-$length/$chr-$length-sorted"
-#rm $chr-$length/$chr-$length.bam
+##rm $chr-$length/$chr-$length.bam
 samtools mpileup -f $genome "$chr-$length/$chr-$length-sorted.bam" > "$chr-$length/$chr-$length.pileup"
-#cleanup
-rm $chr-$length/$chr-$length*.b*
+##cleanup
+#rm $chr-$length/$chr-$length*.b*
 echo | awk -v var=${length} '{if($4>=0.05*var+1){print$1"\t"$2"\t"$4}}' "$chr-$length/$chr-$length.pileup" > "$chr-$length/$chr-$length.map"
-# cleanup
-rm $chr-$length/$chr-$length.pileup
+## cleanup
+#rm $chr-$length/$chr-$length.pileup
 perl "$perl_dir/bedcompact.pl" "$chr-$length/$chr-$length.map" "$chr-$length/$chr-$length.map2"
-# cleanup
-rm $chr-$length/$chr-$length.map
+## cleanup
+#rm $chr-$length/$chr-$length.map
 awk '{if($4==0){print$1"\t"$2"\t"$3"\t"$4}}' "$chr-$length/$chr-$length.map2" > "$chr-$length/$chr-$length.bed"
-# cleanup
-rm $chr-$length/$chr-$length.map2
+## cleanup
+#rm $chr-$length/$chr-$length.map2
 
 echo "Mappability file: $chr-$length/$chr-$length.bed"
 echo "Done!"
